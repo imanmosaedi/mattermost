0d38124c608c57dee0d4631ad06fbbd9467d779c
Iman Mosaedi <imanmosaedi@users.noreply.github.com>
2026-04-22 18:55:10 +0330

    adding iransans font

--- webapp/channels/src/actions/views/root.ts
+++ webapp/channels/src/actions/views/root.ts
@@ -60,7 +60,6 @@ export function loadTranslations(locale: string, url: string): ActionFuncAsync {
 
             try {
                 document.documentElement.dir = isRTL(locale) ? 'rtl' : 'ltr';
-                window.location.reload();
             } catch (error) {
                 console.error(error); //eslint-disable-line no-console
             }
@@ -68,7 +67,6 @@ export function loadTranslations(locale: string, url: string): ActionFuncAsync {
         else {
             try {
                 document.documentElement.dir = 'ltr';
-                window.location.reload();
             } catch (error) {
                 console.error(error); //eslint-disable-line no-console
             }
